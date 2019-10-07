dfe296f5e6621c8c179f90828b8768c5f22c0465
JarekSopinski <jarek.sopinski@gmail.com>
2019-10-07 18:56:54 +0200

    Added bootstrap

--- playground/src/app/courses.component.ts
+++ playground/src/app/courses.component.ts
@@ -4,21 +4,9 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'courses',
     template: `
-        <h2>{{ title }}</h2>
-        <ul>
-            <li *ngFor="let course of courses">
-                {{ course }}
-            </li>
-        </ul>
+        <button class="btn btn-primary">Save</button>
         `
 })
 export class CoursesComponent {
-
-    title = "List of courses";
-    courses;
-
-    constructor(service: CoursesService){
-        this.courses = service.getCourses();
-    }
 
 }
