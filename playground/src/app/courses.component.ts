1c975c3bcc5f94e96c9a88c91f6a40ffeacb102e
JarekSopinski <jarek.sopinski@gmail.com>
2019-10-07 19:54:37 +0200

    Custom pipe exercise

--- playground/src/app/courses.component.ts
+++ playground/src/app/courses.component.ts
@@ -4,19 +4,9 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'courses',
     template: `
-        {{ course.title | uppercase }} <br/>
-        {{ course.students | number }} <br/>
-        {{ course.rating | number:'1.2-2' }} <br/>
-        {{ course.price | currency:'AUD':true:'3.2-2' }} <br/>
-        {{ course.releaseDate | date:'shortDate' }} <br/>
+        {{ text | summary:10 }}
         `
 })
 export class CoursesComponent {
-    course = {
-        title: "The Complete Angular Course",
-        rating: 4.9745,
-        students: 30123,
-        price: 190.95,
-        releaseDate: new Date(2016, 3, 1)
-    }
+    text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
 }
