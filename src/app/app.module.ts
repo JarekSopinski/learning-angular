dbd74fed15e15343c86b22b6895f745387a3f74f
JarekSopinski <jarek.sopinski@gmail.com>
2019-10-11 04:56:17 +0200

    Reorganized fiels in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses.component';
-import { CourseComponent } from './course/course.component';
-import { CoursesService } from './courses.service';
-import { AuthorsComponent } from './authors/authors.component';
-import { AuthorsService } from './authors.service';
-import { SummaryPipe } from './summary.pipe';
-import { FavoriteComponent } from './favorite/favorite.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { CoursesService } from './services/courses.service';
+import { AuthorsService } from './services/authors.service';
+
+import { SummaryPipe } from './pipes/summary.pipe';
+
+import { AppComponent } from './app.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { AuthorsComponent } from './components/authors/authors.component';
+import { FavoriteComponent } from './components/favorite/favorite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent,
     AuthorsComponent,
     SummaryPipe,
     FavoriteComponent
