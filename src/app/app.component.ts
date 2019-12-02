3ca2f4652be4115b6c7ac13221e3435cb8d3151e
JarekSopinski <jarek.sopinski@gmail.com>
2019-12-02 18:49:09 +0100

    Directives - ngFor and Trackby

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,20 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
-  courses = [
-    { id: 1 , name: 'course1' },
-    { id: 2 , name: 'course2' },
-    { id: 3 , name: 'course3' }
-  ];
+  courses;
 
-  onAdd() {
-    this.courses.push({ id: 4, name: 'course4' })
+  loadCourses() {
+
+    this.courses = [
+      { id: 1 , name: 'course1' },
+      { id: 2 , name: 'course2' },
+      { id: 3 , name: 'course3' }
+    ];
+
   }
 
-  onChange(course) {
-    // let index = this.courses.indexOf(course);
-    // this.courses.splice(index, 1);
-    course.name = 'UPDATED';
+  trackCourse(index, course) {
+    return course ? course.id : undefined;
   }
 
 }
