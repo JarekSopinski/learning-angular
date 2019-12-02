b300cf15de7fad05f649ac07630e902d19d83bef
JarekSopinski <jarek.sopinski@gmail.com>
2019-12-02 18:15:56 +0100

    Directives - ngFor and change detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,20 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
-  viewMode = 'somethingElse';
+  courses = [
+    { id: 1 , name: 'course1' },
+    { id: 2 , name: 'course2' },
+    { id: 3 , name: 'course3' }
+  ];
+
+  onAdd() {
+    this.courses.push({ id: 4, name: 'course4' })
+  }
+
+  onChange(course) {
+    // let index = this.courses.indexOf(course);
+    // this.courses.splice(index, 1);
+    course.name = 'UPDATED';
+  }
 
 }
