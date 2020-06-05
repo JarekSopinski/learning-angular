bf6baa19e96384f1eb8ffcf675d5c97394dd54ca
JarekSopinski <jarek.sopinski@gmail.com>
2020-06-05 20:59:39 +0200

    Added FormBuilder example

--- src/app/components/new-course-form/new-course-form.component.ts
+++ src/app/components/new-course-form/new-course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component} from '@angular/core';
-import { FormGroup, FormArray, FormControl } from '@angular/forms';
+import { FormGroup, FormArray, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'new-course-form',
@@ -10,21 +10,23 @@ import { FormGroup, FormArray, FormControl } from '@angular/forms';
 export class NewCourseFormComponent {
 
   form = new FormGroup({
+    name: new FormControl('', Validators.required),
+    contact: new FormGroup({
+      email: new FormControl(),
+      phone: new FormControl()
+    }),
     topics: new FormArray([])
   });
 
-  addTopic(topic: HTMLInputElement) {
-    this.topics.push(new FormControl(topic.value));
-    topic.value = '';
-  }
-
-  removeTopic(topic: FormControl) {
-    let index = this.topics.controls.indexOf(topic);
-    this.topics.removeAt(index);
-  }
-
-  get topics(){
-    return this.form.get('topics') as FormArray;
+  constructor(fb: FormBuilder){
+    this.form = fb.group({
+      name: ['', Validators.required], // we pass FormControl args into the array
+      contact: fb.group({
+        email: [],
+        phone: []
+      }),
+      topics: fb.array([])
+    })
   }
 
 }
