d8072c8ff0c0f21881839d5412808c0a27549709
JarekSopinski <jarek.sopinski@gmail.com>
2020-07-02 07:15:33 +0200

    Http services - posts - createPost (POST req example)

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -9,13 +9,30 @@ import { HttpClient } from '@angular/common/http';
 
 export class PostsComponent {
 
-  posts: object;
+  posts:any[] = [];
+  private url: string = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor(http: HttpClient) {
-    http.get('http://jsonplaceholder.typicode.com/posts')
+  constructor(private http: HttpClient){
+    http.get(this.url)
       .subscribe(response => {
-        this.posts = response;
+        for (const property in response) {
+          // temporary fix becuase tut is based on using array...
+          this.posts.push(response[property])
+        }
       })
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post:object = { title: input.value };
+    input.value = '';
+
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        console.log(response);
+        post['id'] = response['id'];
+        this.posts.splice(0, 0, post);
+      })
+
   }
 
 }
