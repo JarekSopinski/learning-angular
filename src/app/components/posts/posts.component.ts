40789f22fb0a1aefdb3aaefda92e32a3ba49ca36
JarekSopinski <jarek.sopinski@gmail.com>
2020-07-18 09:34:16 +0200

    Http services - handling unexpected errors

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -16,39 +16,63 @@ export class PostsComponent implements OnInit {
 
   ngOnInit() {
     this.service.getPosts()
-      .subscribe(response => {
-        for (const property in response) {
-          // temporary fix becuase tut is based on using array...
-          this.posts.push(response[property])
+      .subscribe(
+        response => {
+          for (const property in response) {
+            // temporary fix becuase tut is based on using array...
+            this.posts.push(response[property])
+          }
+        }, 
+        error => {
+          alert ('An unexpected error occurred.');
+          console.log(error);
         }
-      })
+      )
   }
 
   createPost(input: HTMLInputElement) {
     let post:object = { title: input.value };
     input.value = '';
     this.service.createPost(post)
-      .subscribe(response => {
-        console.log(response);
-        post['id'] = response['id'];
-        this.posts.splice(0, 0, post);
-      })
+      .subscribe(
+        response => {
+          console.log(response);
+          post['id'] = response['id'];
+          this.posts.splice(0, 0, post);
+        }, 
+        error => {
+          alert ('An unexpected error occurred.');
+          console.log(error);
+        }
+      )
   }
 
   updatePost(post) {
     this.service.updatePost(post)
-      .subscribe(response => {
-        console.log(response);
-      })
+      .subscribe(
+        response => {
+          console.log(response);
+        }, 
+        error => {
+          alert ('An unexpected error occurred.');
+          console.log(error);
+        }
+      )
   }
 
   deletePost(post) {
     this.service.deletePost(post.id)
-      .subscribe(response => {
-        console.log(response);
-        let index = this.posts.indexOf(post);
-        this.posts.splice(index, 1);
-      })
+      .subscribe(
+        response => {
+          console.log(response);
+          let index = this.posts.indexOf(post);
+          this.posts.splice(index, 1);
+      }, 
+        error => {
+          alert ('An unexpected error occurred.');
+          console.log(error);
+        }
+      )
   }
 
 }
