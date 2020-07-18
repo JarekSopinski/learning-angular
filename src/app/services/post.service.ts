585548813aa302d624b2246580bfda572f146a46
JarekSopinski <jarek.sopinski@gmail.com>
2020-07-18 11:42:36 +0200

    Http services - reusable error handling method

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -16,41 +16,41 @@ export class PostService {
   constructor(private http: HttpClient) { }
 
   getPosts() {
-    return this.http.get(this.url);
+    return this.http.get(this.url).pipe(
+      map(data => { return data; }),
+      catchError(this.handleError)
+    )
   }
 
   createPost(post: object) {
     return this.http.post(this.url, JSON.stringify(post)).pipe(
-      map (data => {
-        return data;
-      }),
-      catchError((error: Response) => {
-        if (error.status === 400) {
-          return throwError(new BadInput(error));
-        }
-        return throwError(new AppError(error));
-      })
+      map(data => { return data; }),
+      catchError(this.handleError)
     )
   }
 
   updatePost(post) {
     const changedPostUrl:string = `${this.url}/${post.id}`;
-    return this.http.patch(changedPostUrl, JSON.stringify({ isRead: true }));
+    return this.http.patch(changedPostUrl, JSON.stringify({ isRead: true })).pipe(
+      map(data => { return data; }),
+      catchError(this.handleError)
+    )
   }
 
   deletePost(id: number) {
     const deletedPostUrl:string = `${this.url}/${id}`;
     return this.http.delete(deletedPostUrl).pipe(
-      map(data => {
-        return data;
-      }),
-      catchError((error: Response) => {
-        if (error.status === 404){
-          return throwError(new NotFoundError());
-        }
-        return throwError(new AppError(error));
-      })
+      map(data => { return data; }),
+      catchError(this.handleError)
     )
+  }
+
+  private handleError(error: Response) {
+    switch(error.status){
+      case (404): return throwError(new NotFoundError());
+      case (400): return throwError(new BadInput(error));
+      default: return throwError(new AppError(error));
+    }
   }
 
 }
